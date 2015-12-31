63fa90809f33d28a794174eba415fe74674a9fe2
Karthik M A M <karthik_m_a_m@outlook.com>
2015-12-31 22:40:11 +0530

    Optimized for improved efficiency
    Instead of using an iFrame which is very heavy, now the script directly
    downloads the page and uses the native script of the website to get the
    video link. Hence, the efficiency is improved 10 fold

--- Links Grabber.js
+++ Links Grabber.js
@@ -1,48 +1,72 @@
-/*-----------------------------------------------------------------------------------
+/*-----------------------------------------------------------------------------------------------------------
 
 	Author				: Karthik M A M
 	Version				: 2.0
-	Websites Supported	: *http://www.kissanime.com
-						  *http://www.kisscartoon.me
-						  *http://www.kissasian.com
+	Websites Supported	: 1. http://www.kissanime.com
+						  2. http://www.kisscartoon.me
+						  3. http://www.kissasian.com
+						  
+	How to use?			: 1. Go to the episode ith page
+						  2. Press F12 and to open the Browser's console
+						  3. Copy this script and press enter
+						  4. Wait till all the results page appears
+	References			: 1. "https://kissanime.to/Scripts/asp.js" for getting video links
 							
 
------------------------------------------------------------------------------------*/
+-----------------------------------------------------------------------------------------------------------*/
 
 //General Selectors
-var QUALITY_SELECTOR = "#divDownload > a:nth-child(2)"; //Change the index to the req. quality
 var TITLE_SELECTOR = "#navsubbar > p > a";
-var NEXT_BUTTON_ID = "btnNext";
+var DWN_SCRIPT_SEL = "#divDownload script";
+var NEXT_BUTTON_ID = "#btnNext";
+var QUALITY_SELECT = 0;				//Select which video file you want to download
 
 //Global variables
 var dwnlLinks = [];
 var isComplete = false;
+var nextPage = document.location.href;
+var episodeTitle = nextPage
+						.split("/")[4]
+						.split("-")
+						.join(" ") + " -.mp4";
 
-//Create a new frame from where we can navigate to the successive pages
-var tempFrame = document.createElement("iframe");
-tempFrame.src = document.location.href;
-tempFrame.height = "0";
-tempFrame.width = "0";
-document.getElementsByTagName("body")[0].appendChild(tempFrame);
-
-
-//When the iframe loads the page get the download links from ita
-tempFrame.onload = function () {
-	//Get the document of the iFrame
-	var tempDoc = tempFrame.contentWindow.document;
+function documentReady(data) {
+	
+	//Get the document of the from the downloaded data
+	var tempDoc = document.createElement("html");
+	tempDoc.innerHTML = data;
 
 	if (!isComplete) {
-		//Get the download link
-		dwnlLinks.push(tempDoc.querySelector(QUALITY_SELECTOR).href);
+		//Get the episode number
+		var episodeId = nextPage
+							.split("/")[5]
+							.split("?")[0]
+							.split("-")[1];
+		
+		//Get the download links and select the quality
+		var dwnlLink = document.createElement("li");
+		dwnlLink.innerHTML = asp.wrap(tempDoc
+										.querySelector(DWN_SCRIPT_SEL)
+										.innerHTML
+										.split('"')[1]);
+		dwnlLink.getElementsByTagName("a")[0].innerHTML = episodeTitle.replace("-", episodeId);
+		dwnlLink.innerHTML = dwnlLink.getElementsByTagName("a")[QUALITY_SELECT].outerHTML;
+		dwnlLinks.push(dwnlLink);
 		
 		//Traverse to the next page if available
 		//Otherwise navigate to the title page
-		if (tempDoc.getElementById(NEXT_BUTTON_ID) != null) {
-			tempDoc.getElementById(NEXT_BUTTON_ID).click();
+		if (tempDoc.querySelector(NEXT_BUTTON_ID) != null) {
+			nextPage = tempDoc
+							.querySelector(NEXT_BUTTON_ID)
+							.parentElement
+							.getAttribute("href");
 		} else {
 			isComplete = true;
-			tempDoc.location = tempDoc.querySelector(TITLE_SELECTOR).href;
+			nextPage = tempDoc.querySelector(TITLE_SELECTOR).href;
 		}
+		
+		//Get the next page
+		$.get(nextPage, documentReady);
 	} else {
 		//Create a new HTML document for displaying the results
 		writeDoc(dwnlLinks, tempDoc);
@@ -52,30 +76,31 @@ tempFrame.onload = function () {
 function writeDoc(episodeList, tempDoc) {
 	//Document Printer's Selectors
 	var LFT_ADS_DIVS = "#leftside > center";
-	var COMMENTS_DIV = "#leftside > div:nth-child(7)";
+	var COMMENT_DIV1 = "#leftside > div:nth-child(7)";
+	var COMMENT_DIV2 = "#leftside > div:nth-child(8)";
 	var EPISODE_LIST = "#leftside > div:nth-child(4) > div.barContent.episodeList > div:nth-child(2)";
 	var RIT_ADS_DIVS = "#rightside > div:nth-child(5)";
 	
-	//Remove the unwanted contents
-	try {
-		tempDoc.querySelector(COMMENTS_DIV).innerHTML = "";
-		tempDoc.querySelector(EPISODE_LIST).innerHTML = "";
-		tempDoc.querySelector(LFT_ADS_DIVS).innerHTML = "";
-		tempDoc.querySelector(RIT_ADS_DIVS).innerHTML = "";	
-	} catch (error) {
-		console.log("Error: Change the Print Selectors to suit this website");
-	}
-	
 	//Create a new container to hold the new list of episodes
-	var divEpisodes = tempDoc.createElement("div");
-	var episodeHTML = "<ol>";
+	var divEpisodes = document.createElement("div");
+	var episodeHTML = "<ul>";
 	for (var i in episodeList) {
-		episodeHTML += "<li><a target='_blank' href='" + episodeList[i] + "'>" + episodeList[i] + "</a></li>";
+		episodeHTML += episodeList[i].outerHTML;
 	}
-	episodeHTML += "</ol>";
+	episodeHTML += "</ul>";
 	divEpisodes.innerHTML = episodeHTML;
 	tempDoc.querySelector(EPISODE_LIST).appendChild(divEpisodes);
 	
+	//Remove the unwanted contents
+	try {
+		tempDoc.querySelector(COMMENT_DIV1).innerHTML = "";
+		tempDoc.querySelector(EPISODE_LIST).innerHTML = divEpisodes.outerHTML;
+		tempDoc.querySelector(LFT_ADS_DIVS).innerHTML = "";
+		tempDoc.querySelector(RIT_ADS_DIVS).innerHTML = "";
+		tempDoc.querySelector(COMMENT_DIV2).innerHTML = "";	
+	} catch (error) {
+		console.log("Error: Change the Print Selectors to suit this website");
+	}
 	
 	//Create the html document using the original document as a template
 	var docHTML = "<html>"
@@ -87,3 +112,6 @@ function writeDoc(episodeList, tempDoc) {
 				 +"</html>";
 	document.write(docHTML);	
 }
+
+//Start grabbing the download link
+$.get(nextPage, documentReady);
